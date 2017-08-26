5aa5e70b4a297728043d4701c1f65cf30043d790
RobertoGalois <victor.vague@live.fr>
2017-08-27 00:50:55 +0200

    Create ft_print_combn.h

---
+++ ft_print_combn.h
@@ -0,0 +1,6 @@
+ifndef FT_PRINT_COMBN_H
+ #define FT_PRINT_COMBN_H
+ void   ft_print_number(int *p_tab, int p_nb_arr);
+ void   ft_print_n_numbers(int* p_tab, int p_foc_indx, int p_nb_indx);
+ void   ft_print_combn(int nb); 
+#endif
