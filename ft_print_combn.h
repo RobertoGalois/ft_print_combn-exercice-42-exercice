8db170a58085ce48af344cd43d10d44679b076e0
RobertoGalois <victor.vague@live.fr>
2017-08-27 15:06:20 +0200

    Update ft_print_combn.h

--- ft_print_combn.h
+++ ft_print_combn.h
@@ -1,4 +1,4 @@
-ifndef FT_PRINT_COMBN_H
+#ifndef FT_PRINT_COMBN_H
  #define FT_PRINT_COMBN_H
  void   ft_print_number(int *p_tab, int p_nb_arr);
  void   ft_print_n_numbers(int* p_tab, int p_foc_indx, int p_nb_indx);
